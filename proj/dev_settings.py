268bdb53bdbcdb627c02a71317023ea6a0d0f4ea
nanjiangshu <nanjiang.shu@gmail.com>
2019-12-12 13:49:39 +0100

    update settings

--- proj/dev_settings.py
+++ proj/dev_settings.py
@@ -2,33 +2,37 @@
 Django settings for proj project in development.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/topics/settings/
+https://docs.djangoproject.com/en/2.2.7/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.11/ref/settings/
+https://docs.djangoproject.com/en/2.2.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+from libpredweb import myfunc
+from libpredweb import webserver_common as webcom
 try:
     from .shared_settings import *
 except ImportError:
     pass
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
-
-# SECURITY WARNING: keep the secret key used in production secret!
+# See https://docs.djangoproject.com/en/2.2.7/howto/deployment/checklist/
 SECRET_KEY = '5&!cq9#+(_=!ou=mco0=-qrmn6h66o(f)h$ho4+0vo1#d24xdy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
+ALLOWED_HOSTS = []
+allowed_host_file = "%s/allowed_host_dev.txt"%(BASE_DIR)
+computenodefile = "%s/pred/config/computenode.txt"%(BASE_DIR)
+for f in [allowed_host_file, computenodefile]:
+    if os.path.exists(f):
+        ALLOWED_HOSTS +=  myfunc.ReadIDList2(f,col=0)
 
-ALLOWED_HOSTS = ['localhost', 'boctopus.topcons.net', 'b2.topcons.net', 'www.b2.topcons.net', 'boctopus.cbr.su.se', 'boctopus.bioinfo.se', 'dev.boctopus.bioinfo.se']
-
-STATIC_ROOT = "/var/www/html/boctopus2/proj/pred/static"
+# add also the host ip address
+hostip = webcom.get_external_ip()
+ALLOWED_HOSTS.append(hostip)
 
--- proj/pro_settings.py
+++ proj/pro_settings.py
@@ -2,16 +2,17 @@
 Django settings for proj project in production
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
+https://docs.djangoproject.com/en/2.2.7/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
+https://docs.djangoproject.com/en/2.2.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+from libpredweb import myfunc
+from libpredweb import webserver_common as webcom
 try:
     from .shared_settings import *
 except ImportError:
@@ -20,20 +21,18 @@ except ImportError:
 with open('/etc/django_pro_secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-TEMPLATE_DEBUG = DEBUG
+ALLOWED_HOSTS = []
+allowed_host_file = "%s/allowed_host_pro.txt"%(BASE_DIR)
+computenodefile = "%s/pred/config/computenode.txt"%(BASE_DIR)
+for f in [allowed_host_file, computenodefile]:
+    if os.path.exists(f):
+        ALLOWED_HOSTS +=  myfunc.ReadIDList2(f,col=0)
 
-ALLOWED_HOSTS = ['localhost', 'boctopus.topcons.net', 'b2.topcons.net', 'www.b2.topcons.net', 'boctopus.cbr.su.se', 'boctopus.bioinfo.se']
-
-computenodefile = "%s/pred/static/computenode.txt"%(BASE_DIR)
-if os.path.exists(computenodefile):
-    nodelist = []
-    try:
-        nodelist = open(computenodefile, "r").read().split()
-    except:
-        pass
-    ALLOWED_HOSTS += nodelist
-
+# add also the host ip address
+hostip = webcom.get_external_ip()
+ALLOWED_HOSTS.append(hostip)
 
