459b95e0a2df37fae98239881d065c1140cb365c
nanjiangshu <nanjiang.shu@gmail.com>
2020-03-14 21:07:53 +0100

    remove paths for python2

--- proj/pred/app/qd_fe.py
+++ proj/pred/app/qd_fe.py
@@ -12,9 +12,6 @@ webserver_root = os.path.realpath("%s/../../../"%(rundir))
 activate_env="%s/env/bin/activate_this.py"%(webserver_root)
 exec(compile(open(activate_env, "rb").read(), activate_env, 'exec'), dict(__file__=activate_env))
 #Add the site-packages of the virtualenv
-site.addsitedir("%s/env/lib/python2.7/site-packages/"%(webserver_root))
-sys.path.append("%s/env/lib/python2.7/site-packages/"%(webserver_root))
-sys.path.append("/usr/local/lib/python2.7/dist-packages")
 
 from libpredweb import myfunc
 from libpredweb import dataprocess
